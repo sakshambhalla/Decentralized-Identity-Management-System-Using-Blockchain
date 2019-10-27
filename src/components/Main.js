bb74941389bf371053238b7ffee9e453c99ce875
sakshambhalla <sakshambhalla11@gmail.com>
2019-10-27 16:18:56 +0530

    optimised code

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,5 @@
 import React, { Component } from 'react';
-import Web3 from 'web3';
 class Main extends Component {
-
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.ethereum.autoRefreshOnNetworkChange = false;
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
-  }
-
-  async loadBlockchainData() {
-    const web3 = window.web3;
-    const accounts = await web3.eth.getAccounts();
-    return accounts[0];
-  }
-
-
   render() {
     return (
       <div id="content">

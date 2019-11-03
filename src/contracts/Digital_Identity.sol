dab7cbfd07b501a92d6eadaedef3e1e7e8067377
vinayd1 <agrawalv4@gmail.com>
2019-11-03 20:36:35 +0530

    PRE-FINAL

--- src/contracts/Digital_Identity.sol
+++ src/contracts/Digital_Identity.sol
@@ -1,7 +1,6 @@
 pragma solidity 0.5.0;
-import "./provableAPI.sol";
 
-contract Digital_Identity is usingProvable {
+contract Digital_Identity {
   string public name = "Digital Identity Blockchain";
   mapping(address => Identity) public identities;
 
@@ -10,16 +9,8 @@ contract Digital_Identity is usingProvable {
     string contentAddress;
   }
 
-  event Received(string data);
-
   function createIdentity(string memory _contentAddress) public {
     require(bytes(_contentAddress).length > 0, 'Invalid address');
-    //provable_query("IPFS", "QmYokvq4GYMFAHWxzj4aYQ7tAn6FnNNWM4iMfEE1b7TECd");
     identities[msg.sender] = Identity(msg.sender, _contentAddress);
   }
-
-  // function retrieveIdentity(uint _id) public {
-  //   require(_id > 0 && _id <= identityCount, 'Invalid product ID');
-  //   //Identity memory _identity = identities[_id];
-  // }
 }
